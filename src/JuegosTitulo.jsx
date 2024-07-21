0bcce9b566a07e78c7f06337714b1a6fe894d198
Lesky20 <unionenrique4@gmail.com>
2024-07-20 21:55:41 -0400

    dia 2

--- src/JuegosTitulo.jsx
+++ src/JuegosTitulo.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 const JuegosTitulo = () => {
     const url = "http://45.236.130.191/juegos.php"
-    const [juego, setJuegos] = useState([])
+    const [juegos, setJuegos] = useState([])
 
     useEffect(() => {
       fetch(url).then((response) => response.json()).then((datos) => setJuegos(datos))
@@ -12,7 +12,7 @@ const JuegosTitulo = () => {
     <>      
         <h1>Titulo De Los Juegos</h1>
         <ul>
-          {juego.map(u => <li>{u.juegos.titulo}</li>)}
+          {juegos.map(u => <li>{u.juegos.titulo}</li>)}
         </ul>
     </>
   )
