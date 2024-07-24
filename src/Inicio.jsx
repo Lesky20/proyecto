30948ebae09951be58790f6c6524ef99444634ab
Lesky20 <unionenrique4@gmail.com>
2024-07-24 12:45:04 -0400

    dia 5.1

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -11,6 +11,21 @@ const Inicio = () => {
     return (
         <div>
           <h1>Bienvenido A Nuestra Pagina De Videojuegos :D</h1>
+          <div className='col '>
+            <label htmlFor="id" className='form-label'>ID: </label>
+            <input type="number" id="id" className='form-control'/>
+          </div>
+          <div className="col">
+            <label htmlFor="genero" className='form-label'>Genero: </label>
+            <input type="text" id="genero" className='form-control'/>
+          </div>
+          <div className="col">
+            <label htmlFor="juegos" className='form-label'>Juegos: </label>
+            <input type="text" id="juegos" className='form-control'/>
+          </div>
+          <div className="col">
+            <button>Guardar</button>
+          </div>
           <Router>
             <ul>
               <nav>
